0bfd542cc479f0aba6e97578eca8d3ff23bd4c6b
ayoub-bougroune <68024921+ayoub-bougroune@users.noreply.github.com>
2022-01-10 12:06:31 +0100

    Delete weather-bakend directory

--- weather-bakend/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "secret" : "secretkeymeteoapp",
-    "mongoDbUrl" : "mongodb://localhost/db",
-    "appid" : "1416ad2521f18a5532f27b60a5581d9a",
-    "urlApiWeatherDay" : "https://api.openweathermap.org/data/2.5/group",
-    "urlApiWeatherWeek" : "https://api.openweathermap.org/data/2.5/forecast"
-}
